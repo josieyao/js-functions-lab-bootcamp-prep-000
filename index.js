1fb0d2402759820c768a843b0fcb9b9dfac3aad6
Josephine Yao <yao.josephine@yahoo.com>
2019-01-08 05:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
+<<<<<<< HEAD
 function happyHolidays(){
 return "Happy holidays!"
 }
@@ -13,4 +14,8 @@ function happyHolidaysTo(name) {
 
  function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
+=======
+function shout(string) {
+  return string.toUpperCase()
+>>>>>>> 36eb1114bf8d0b7db149e8dc5bc0ae80034782d3
 }
